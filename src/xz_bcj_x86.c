88fcb18f88dfe08e183ed5e99060fa80408ac9fa
Hiroshi Miura <miurahr@linux.com>
2020-07-12 19:22:47 +0900

    Revert macro as original
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/xz_bcj_x86.c
+++ src/xz_bcj_x86.c
@@ -1,5 +1,6 @@
 #include "xz_private.h"
 
+#define Test86MSByte(b) ((b) == 0 || (b) == 0xFF)
 
 static size_t
 x86_code(simple_x86 *simple, uint32_t now_pos, bool is_encoder,
@@ -45,7 +46,7 @@ x86_code(simple_x86 *simple, uint32_t now_pos, bool is_encoder,
 
 		b = buffer[buffer_pos + 4];
 
-		if (bcj_x86_test_msbyte(b)
+		if (Test86MSByte(b)
 			&& MASK_TO_ALLOWED_STATUS[(prev_mask >> 1) & 0x7]
 				&& (prev_mask >> 1) < 0x10) {
 
@@ -71,7 +72,7 @@ x86_code(simple_x86 *simple, uint32_t now_pos, bool is_encoder,
 
 				b = (uint8_t)(dest >> (24 - i * 8));
 
-				if (!bcj_x86_test_msbyte(b))
+				if (!Test86MSByte(b))
 					break;
 
 				src = dest ^ ((1U << (32 - i * 8)) - 1);
@@ -88,13 +89,11 @@ x86_code(simple_x86 *simple, uint32_t now_pos, bool is_encoder,
 		} else {
 			++buffer_pos;
 			prev_mask |= 1;
-			if (bcj_x86_test_msbyte(b))
-				prev_mask |= 0x10;
 		}
 	}
 
 	simple->prev_mask = prev_mask;
-	simple->prev_pos = prev_pos;
+	simple->prev_pos = now_pos + (uint32_t)(buffer_pos);
 
 	return buffer_pos;
 }
